3265808a1058544fa54bcdecb8390b76571013c3
Amanda Ormerod <125412217+speedy-87@users.noreply.github.com>
2025-01-17 21:02:59 -0500

    Add files via upload

--- src/pages/BuyPreSale.js
+++ src/pages/BuyPreSale.js
@@ -8,9 +8,9 @@ function Presale() {
         <h2>Buy $CALAPES Presale</h2>
 
         <p className="calTagline">
-         You will need to visit pinksale website to claim their tokens on Launch day! 
+         You will need to visit pinksale website to claim your tokens on Launch day! 
          <br /> 
-         (Our vc will be open for any help or support needed)
+         (Our voice chat will be open for any help or support needed)
         </p>
         <p className="calTagline"></p>
         <a href="https://www.pinksale.finance/solana/launchpad/2F1WZUHWkeKhJgMsCRKsVTwBN79qzZnb5kzPk3FmXHLu" className="enterCAL" target="_blank" rel="noopener noreferrer">
